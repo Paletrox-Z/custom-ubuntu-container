cfc1681db5c0de05651aca543a1750d07154fae1
Paletrox-Z <paletroxzpz@gmail.com>
2024-11-06 18:54:49 +0530

    Adding support for mounting a folder from host to container

--- build-deploy.bat
+++ build-deploy.bat
@@ -1,3 +1,3 @@
 @echo off
 docker build -t custombuntu:latest .
-docker run -p "5901:5901" --rm --name custombuntu -it custombuntu:latest
+docker run -p "5901:5901" --rm --name custombuntu --mount src=%cd%\shared_folder,target=/home/nonroot/shared_folder,type=bind -it custombuntu:latest
--- build-deploy.sh
+++ build-deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t custombuntu:latest .
-docker run -p "5901:5901" --rm --name custombuntu -it custombuntu:latest
+docker run -p "5901:5901" --rm  --name custombuntu --mount src=$(pwd)/shared_folder,target=/home/nonroot/shared_folder,type=bind -it custombuntu:latest
